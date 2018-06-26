c7d60f051d8d5aadf4e8dfa5b612d3e7893e27f2
timvan <timvanellemeet@gmail.com>
2018-06-26 17:14:46 +1000

    config

--- app/application.py
+++ app/application.py
@@ -16,6 +16,11 @@ def get_issues():
     maxResults = 100
     startAt = 0
 
+    with open('jira_config.json', 'r') as jsonfile:
+        config = json.load(jsonfile)
+    
+    headers = config['headers']
+    
     querystring = {
         "jql": "project = ADS order by created ASC",
         # "jql": "project = ADS AND type in standardIssueTypes()",
@@ -24,11 +29,6 @@ def get_issues():
         # "fields": "status, subtasks, issuetype, summary, aggregatetimespent, aggregatetimeoriginalestimate, aggregatetimeestimate, customfield_10016, assignee, created, timespent, timeoriginalestimate, timeestimate, resolutiondate, fixVersions",
         "fields" : "",
         "expand": "changelog, renderedFields",
-    }
-
-    headers = {
-        'Authorization': "Basic dGltLnZhbi5lbGxlbWVldDpBZ3JpZGlnaXRhbDEhamlyYQ==",
-        'Cache-Control': "no-cache",    
     }
 
     print("-" * 10)
@@ -62,11 +62,11 @@ def get_issues():
 def get_all_sprints():
 
     url = 'https://fullprofile.atlassian.net/rest/agile/1.0/board/12/sprint/'
-    headers = {
-        'Authorization': "Basic dGltLnZhbi5lbGxlbWVldDpBZ3JpZGlnaXRhbDEhamlyYQ==",
-        'Cache-Control': "no-cache",
-    }
 
+    with open('jira_config.json', 'r') as jsonfile:
+        config = json.load(jsonfile)
+    
+    headers = config['headers']
     querystring = {
         'startAt': 0,
     }
@@ -124,10 +124,6 @@ def format_issue(i):
     issue['rootcause'] = i['fields']['customfield_11222']['value'] if i['fields']['customfield_11222'] else None
     # issue['impactedCustomers'] = i['fields']['customfield_11223'] # MANY TO MANY TABLE REQD
     # issue['description'] = i['fields']['description'] # NOT NEEDED AT THIS STAGE
-    # issue['fixVersions'] =i['fields']['fixVersions'] # MANY TO MANY TABLE REQD
-    # issue['fixVersionsName'] =i['fields']['fixVersions']['name']
-    # issue['fixVersionsDescription'] =i['fields']['fixVersions']['description']
-    # issue['fixVersionsReleaseDate'] =i['fields']['fixVersions']['releaseDate']
     issue['issuetype'] = i['fields']['issuetype']['name']
     issue['subtask'] = i['fields']['issuetype']['subtask']
     # issue['labels'] = i['fields']['labels'] # LIST TABLE MAYBE REQD
@@ -154,6 +150,9 @@ def format_issue(i):
     issue['workratio'] = i['fields']['workratio']
 
 
+    # The following fields are not native to the Jira API
+    # Datetime objects above have also non native fields with suffixes '_week' where datetitme is formatted into YYYY_ww - whwere ww is week number
+    # storySubtaskKey is also non native - parent and it's subtask have the parent key
 
     try:
         if i['fields']['summary'].upper().startswith(("BACK", "API", "PERI"), 1):
@@ -169,15 +168,17 @@ def format_issue(i):
     except:
         issue['devteam'] = None
 
-
-
+    if i['fields']['resolutiondate']:
+        issue['resolutionDateInSprint'] = in_which_sprint(i, dateutil.parser.parse(i['fields']['resolutiondate']))
+    else:
+        issue['resolutionDateInSprint'] = None
 
 
     return translate_dict('issue_', issue)
 
 def issues_to_csv(issues):
 
-    filename = 'issues_' + datetime.now().strftime("%Y-%m-%d_%H%M") + ".csv"
+    filename = 'data/issues_' + datetime.now().strftime("%Y-%m-%d_%H%M") + ".csv"
 
     with open(filename, 'w') as csvfile:
         
@@ -190,7 +191,7 @@ def issues_to_csv(issues):
 
 # ------------ ISSUES TO SPRINTS MANY TO MANY ------------ #
 
-def sprints_in_issue(i):
+def issue_in_sprints(i):
 
     sprints_raw = i['fields']['customfield_10016']
     
@@ -217,7 +218,7 @@ def sprints_in_issue(i):
 
 def issues_to_sprints_to_csv(issues):
 
-    filename = 'issues_to_sprints_' + datetime.now().strftime("%Y-%m-%d_%H%M") + ".csv"
+    filename = 'data/issues_to_sprints_' + datetime.now().strftime("%Y-%m-%d_%H%M") + ".csv"
 
     with open(filename, 'w') as csvfile:
         
@@ -227,7 +228,7 @@ def issues_to_sprints_to_csv(issues):
         writer.writeheader()
 
         for i in issues:
-            sprints = sprints_in_issue(i)
+            sprints = issue_in_sprints(i)
 
             if sprints:
                 for s in sprints:
@@ -250,11 +251,28 @@ def format_sprint(s):
     sprint['completeDate_week'] = adjust_for_utc(dateutil.parser.parse(s['completeDate'])).strftime("%Y_%W") if 'completeDate' in s else None
     sprint['goal'] = s['goal']
 
+    # sprint['team'] =
+
+    split = s['name'].split()
+    if split[0] == 'Sprint':
+        
+        sprint['number'] = split[1]
+
+        if 'Octopockles' in split:
+            sprint['team'] = 'Octopockles'
+
+        if True in [i.upper() in ['A', 'A-TEAM'] for i in split]:
+            sprint['team'] = 'A-Team'
+        
+    else:
+        sprint['number'] = None
+        sprint['team'] = None
+
     return translate_dict('sprint_', sprint)
 
 def sprints_to_csv(sprints):
 
-    filename = 'sprints_' + datetime.now().strftime("%Y-%m-%d_%H%M") + ".csv"
+    filename = 'data/sprints_' + datetime.now().strftime("%Y-%m-%d_%H%M") + ".csv"
 
     with open(filename, 'w') as csvfile:
         
@@ -267,6 +285,202 @@ def sprints_to_csv(sprints):
             writer.writerow(format_sprint(s))
 
 # ------------ CHANGELOG ------------ #
+
+def format_changelog(i):
+    changelog = i['changelog']
+    changelogFormatted = []
+
+    # Sort current changelog in descending order - created first is now top
+    histories = changelog['histories'][::-1]
+
+    for i_change, change in enumerate(histories):
+        for item in change['items']:
+
+            # Filter changelog for fields
+            # if item['field']:
+            # if item['field'] in ['timespent', 'timeestimate', 'timeoriginalestimate', 'status', 'WorklogId', 'WorklogTimeSpent', 'resolution', 'resolutiondate', 'Sprint']:
+            if item['field'] in ['timespent']:
+            # if item['field'] not in ['description', 'Attachment', 'assignee', 'Parent', 'Fix Version', 'summary']:
+
+                # Check current item in change against all items in previous change for duplicates, if duplicate contiune onto the next item not appending a newItem
+                if i_change > 0:
+                    if (is_item_in_prev_change(item, histories[i_change - 1]['items'])):
+                        #print("Duplicate changelog item detected", item['field'])
+                        continue
+
+                # store new formatted item in formatted changelog
+                newItem = {
+                    'issue_id': i['id'],
+                    'changelog_id': change['id'],
+                    'changelog_created': adjust_for_utc(dateutil.parser.parse(change['created'])).strftime("%d-%m-%Y %H:%M:%S"),
+                    'changelog_created_week': adjust_for_utc(dateutil.parser.parse(change['created'])).strftime("%Y_%W"),
+                    'changelog_field': item['field'],
+                    'changelog_from': item['fromString'],
+                    'changelog_to': item['toString'],
+                    'sprint_id': in_which_sprint(i, dateutil.parser.parse(change['created'])) if i['fields']['customfield_10016'] else None,
+                }
+
+                try:
+                    newItem['changelog_author'] = change['author']['displayName']
+                except:
+                    print('ERROR format_changelog: no display name')
+
+                changelogFormatted.append(newItem)
+
+    #print(json.dumps(changelogFormatted, indent = 4))
+    return changelogFormatted
+
+def changelog_to_csv(issues):
+
+    filename = 'data/issues_changelog_' + datetime.now().strftime("%Y-%m-%d_%H%M") + ".csv"
+
+    with open(filename, 'w') as csvfile:
+        
+        fieldnames = ['issue_id', 'changelog_id', 'changelog_author', 'changelog_created', 'changelog_created_week', 'changelog_field', 'changelog_from', 'changelog_to', 'sprint_id']
+
+        writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
+        writer.writeheader()
+
+        for i in issues:
+            changelog = format_changelog(i)
+
+            if changelog:
+                for c in changelog:
+                    writer.writerow(c)
+
+# ------------ ISSUES TO FIXVERSIONS MANY TO MANY ------------ #
+
+def issue_in_fixs(i):
+
+    rows = []
+
+    for fix in i['fields']['fixVersions']:
+        fixrow = {}
+        fixrow['issue_id'] = i['id']
+        fixrow['fixVersions_id'] = fix['id']
+        rows.append(fixrow)
+    
+    return rows
+
+def issues_to_fixVersions_to_csv(issues):
+
+    filename = 'data/issues_to_fixVersions_' + datetime.now().strftime("%Y-%m-%d_%H%M") + ".csv"
+
+    with open(filename, 'w') as csvfile:
+
+        fieldnames = ['issue_id', 'fixVersions_id']
+
+        writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
+        writer.writeheader()
+
+        for i in issues:
+            if len(i['fields']['fixVersions']) > 0:
+              for f in issue_in_fixs(i):
+                    writer.writerow(f)
+
+# ------------ FIXVERSIONS ------------ #
+
+def format_fix(f):
+
+    fix = {}
+
+    fix['id'] = f['id']
+    fix['name'] = f['name']
+    try:
+        fix['description'] = f['description']
+    except:
+        fix['description'] = None
+
+    fix['releaseDate'] = f['releaseDate']
+
+    return translate_dict('fixVersions_', fix)
+
+    # issue['fixVersions'] =i['fields']['fixVersions'] # MANY TO MANY TABLE REQD
+    # issue['fixVersionsName'] =i['fields']['fixVersions']['name']
+    # issue['fixVersionsDescription'] =i['fields']['fixVersions']['description']
+    # issue['fixVersionsReleaseDate'] =i['fields']['fixVersions']['releaseDate']
+
+def fixVersions_to_csv(issues):
+
+    fixes = []
+
+    for i in issues:
+        for f in i['fields']['fixVersions']:
+            cond = True in [f['id'] in fx.values() for fx in fixes]
+            if not cond:
+                fixes.append(format_fix(f))
+
+    filename = 'data/fixVersions' + datetime.now().strftime("%Y-%m-%d_%H%M") + ".csv"
+
+    with open(filename, 'w') as csvfile:
+        
+        keys = fixes[0].keys()
+        writer = csv.DictWriter(csvfile, fieldnames = keys)
+        writer.writeheader()
+
+        for f in fixes:
+            writer.writerow(f)
+
+# ------------ IMPACTED CUSTOMERS ------------ #
+
+def issues_to_impactedCustomers_to_csv(issues):
+
+    filename = 'data/issues_to_impactedCustomers_' + datetime.now().strftime("%Y-%m-%d_%H%M") + ".csv"
+
+    with open(filename, 'w') as csvfile:
+
+        fieldnames = ['issue_id', 'impactedCustomer']
+
+        writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
+        writer.writeheader()
+
+        for i in issues:
+            if i['fields']['customfield_11223']:
+                for c in i['fields']['customfield_11223']:
+
+                    row = {
+                        'issue_id': i['id'],
+                        'impactedCustomer': c,
+                    }
+
+                    writer.writerow(row)
+
+# ------------ HELPERS ------------ #
+
+def translate_dict(prefix, item):
+
+    new_item = {}
+
+    for k, v in item.items():
+
+        new_k = prefix + k
+        new_item[new_k] = v
+
+    return new_item
+
+def write_data_to_file(file_name, data):
+
+    with open(file_name, 'w') as f:
+        json.dump(data, f)
+
+def is_item_in_prev_change(item, prev_change):
+    """
+    Check current item against all items in previous change, return True if current item is a duplicate item else return False
+    """
+    for prev_item in prev_change:
+        #print("checking", item, "\nagainst", prev_item)
+        if prev_item['field'] == item['field'] and item['from'] == prev_item['from'] and item['to'] == prev_item['to']:
+            return True
+
+    return False
+
+def adjust_for_utc(datetime_obj):
+
+    d = datetime_obj
+    d_as_utc = (d - d.utcoffset()).replace(tzinfo=dateutil.tz.tzutc())
+    d_as_local = d_as_utc.astimezone(dateutil.tz.tzlocal())
+
+    return d_as_local
 
 def in_which_sprint(issue, event_date):
 
@@ -334,131 +548,22 @@ def in_which_sprint(issue, event_date):
     if event_date < sprintsFormatted[0]['startDate']:
         return None
     
-
-
     for s in sprintsFormatted:
-
-        if str(issue['id']) == '32924':
-            print(s['id'])
 
         if s['state'] == 'ACTIVE':
             return s['id']
 
         if event_date < s['completeDate']:
-            
-            if str(issue['id']) == '32924':
-                print(event_date, s['completeDate'])
-
             return s['id']
 
-def format_changelog(i):
-    changelog = i['changelog']
-    changelogFormatted = []
-
-    # Sort current changelog in descending order - created first is now top
-    histories = changelog['histories'][::-1]
-
-    for i_change, change in enumerate(histories):
-        for item in change['items']:
-
-            # Filter changelog for fields
-            # if item['field']:
-            # if item['field'] in ['timespent', 'timeestimate', 'timeoriginalestimate', 'status', 'WorklogId', 'WorklogTimeSpent', 'resolution', 'resolutiondate', 'Sprint']:
-            if item['field'] in ['timespent']:
-            # if item['field'] not in ['description', 'Attachment', 'assignee', 'Parent', 'Fix Version', 'summary']:
-
-                # Check current item in change against all items in previous change for duplicates, if duplicate contiune onto the next item not appending a newItem
-                if i_change > 0:
-                    if (is_item_in_prev_change(item, histories[i_change - 1]['items'])):
-                        #print("Duplicate changelog item detected", item['field'])
-                        continue
-
-                # store new formatted item in formatted changelog
-                newItem = {
-                    'issue_id': i['id'],
-                    'changelog_id': change['id'],
-                    'changelog_created': adjust_for_utc(dateutil.parser.parse(change['created'])).strftime("%d-%m-%Y %H:%M:%S"),
-                    'changelog_created_week': adjust_for_utc(dateutil.parser.parse(change['created'])).strftime("%Y_%W"),
-                    'changelog_field': item['field'],
-                    'changelog_from': item['fromString'],
-                    'changelog_to': item['toString'],
-                    'sprint_id': in_which_sprint(i, dateutil.parser.parse(change['created'])) if i['fields']['customfield_10016'] else None,
-                }
-
-                try:
-                    newItem['changelog_author'] = change['author']['displayName']
-                except:
-                    print('ERROR format_changelog: no display name')
-
-                changelogFormatted.append(newItem)
-
-    #print(json.dumps(changelogFormatted, indent = 4))
-    return changelogFormatted
-
-def changelog_to_csv(issues):
-
-    filename = 'issues_changelog_' + datetime.now().strftime("%Y-%m-%d_%H%M") + ".csv"
-
-    with open(filename, 'w') as csvfile:
-        
-        fieldnames = ['issue_id', 'changelog_id', 'changelog_author', 'changelog_created', 'changelog_created_week', 'changelog_field', 'changelog_from', 'changelog_to', 'sprint_id']
-
-        writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
-        writer.writeheader()
-
-        for i in issues:
-            changelog = format_changelog(i)
-
-            if changelog:
-                for c in changelog:
-                    writer.writerow(c)
-
-# ------------ HELPERS ------------ #
-
-def translate_dict(prefix, item):
-
-    new_item = {}
-
-    for k, v in item.items():
-
-        new_k = prefix + k
-        new_item[new_k] = v
-
-    return new_item
-
-def write_data_to_file(file_name, data):
-
-    with open(file_name, 'w') as f:
-        json.dump(data, f)
-
-def is_item_in_prev_change(item, prev_change):
-    """
-    Check current item against all items in previous change, return True if current item is a duplicate item else return False
-    """
-    for prev_item in prev_change:
-        #print("checking", item, "\nagainst", prev_item)
-        if prev_item['field'] == item['field'] and item['from'] == prev_item['from'] and item['to'] == prev_item['to']:
-            return True
-
-    return False
-
-def adjust_for_utc(datetime_obj):
-
-    if str(datetime_obj.tzinfo) == 'tzutc()':
-        return datetime_obj + timedelta(hours = 11)
-
-    else:
-        return datetime_obj
-
-
-
+    return None
 
 # ------------ SETUP ------------ #
 
 if __name__ == "__main__":
 
     PAGINATE = True
-    OFFLINE_MODE = True
+    OFFLINE_MODE = False
     
     if OFFLINE_MODE:
         print('-'*20)
@@ -484,8 +589,11 @@ if __name__ == "__main__":
 
 
 
-    issues_to_csv(issues)
-    issues_to_sprints_to_csv(issues)
-    sprints_to_csv(sprints)
-    changelog_to_csv(issues)
+    # issues_to_csv(issues)
+    # issues_to_sprints_to_csv(issues)
+    # sprints_to_csv(sprints)
+    # changelog_to_csv(issues)
+    # issues_to_fixVersions_to_csv(issues)
+    # fixVersions_to_csv(issues)
+    issues_to_impactedCustomers_to_csv(issues)
 
